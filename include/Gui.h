5d97a3803604d20b051b78082e91d565d1ae6279
James Stevenson <james@stev.org>
2013-07-11 15:24:41 +0100

    Updated License in source files

--- include/Gui.h
+++ include/Gui.h
@@ -1,3 +1,24 @@
+/*
+
+  This file is part of ipstats, a network monitoring utility
+  The original author can be contacted at james@stev.org or http://www.stev.org/
+  The source code can be found at https://github.com/mistralol/ipstats
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
 
 
 class Gui
